00a84d41348a2befdd0c3388081b89b8c3077981
Julias Uddin Khan <135951641+julias16@users.noreply.github.com>
2023-08-29 14:46:52 +0600

    Add files via upload

---
+++ contest1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a==2002)
+        printf("Acesso Permitido\n");
+    else
+        printf("Senha Invalida\n");
+    return 0;
+}
