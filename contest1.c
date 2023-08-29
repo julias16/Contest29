ea1afbb018fdac00a3c0b1ce806f9a5282140470
Julias Uddin Khan <135951641+julias16@users.noreply.github.com>
2023-08-29 14:59:31 +0600

    Add files via upload

--- contest1.c
+++ contest1.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d", &a);
-    if(a==2002)
-        printf("Acesso Permitido\n");
-    else
-        printf("Senha Invalida\n");
-    return 0;
+	int a, b=2002, i;
+	for(; ;)
+    {
+        scanf("%d", &a);
+        if(a==b)
+            printf("Acesso Permitido\n");
+        else
+            printf("Senha Invalida\n");
+    }
+	return 0;
 }
